2a9d6166739acb65d43d45901b65d093a4c3630b
Jyoti Rawat <266243325+Jyotiii06@users.noreply.github.com>
2026-03-12 19:14:54 +0530

    Uploaded basic MySQL practice queries
    Added a basic MySQL database creation and table queries.

---
+++ stu_table.mysql.sql
@@ -0,0 +1,13 @@
+create table studetns(
+    stu_id int primary key,
+    f_name varchar(40),
+    l_name varchar(40),
+    age int,
+    course varchar(10),
+);
+insert into students values(34252,'Neece','Goblin',23,'MBA');
+insert into students values(45323,'Alex','Purohit',43,'BCA');
+insert into students values(34252,'Hanish','Thakur',21,'BBA');
+insert into students values(34252,'Jyoti','Rawat',33,'B.Tech');
+desc students;
+select * from students;
