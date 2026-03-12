5e8a7dd5365860bb0910799080c3772b1edda7ec
Jyoti Rawat <266243325+Jyotiii06@users.noreply.github.com>
2026-03-12 19:15:50 +0530

    Update stu_table.mysql.sql

--- stu_table.mysql.sql
+++ stu_table.mysql.sql
@@ -1,4 +1,4 @@
-create table studetns(
+create table students(
     stu_id int primary key,
     f_name varchar(40),
     l_name varchar(40),
@@ -11,3 +11,4 @@ insert into students values(34252,'Hanish','Thakur',21,'BBA');
 insert into students values(34252,'Jyoti','Rawat',33,'B.Tech');
 desc students;
 select * from students;
+
